a3a02a6ad722f2b044fb02da609799a9379c3d75
heruoxin <heruoxin@gmail.com>
2014-10-10 00:00:35 +0800

    native notification should work

--- js/pushcullet/send_notification.js
+++ js/pushcullet/send_notification.js
@@ -1,15 +1,10 @@
-var Notification = require('node-notifier');
-
 module.exports = function(e){
-  var notifier = new Notification();
-  notifier.notify({
-    "title": e.title,
-    //        "subtitle": e.attr("usage"),
-    "message": e.body,
-    //        "sound": "Funk", // case sensitive
-    "contentImage": './icons/'+e.type+'.png',
-    "appIcon": './icons/'+e.type+'.png',
-    //        "open": "file://" + __dirname + "/coulson.jpg"
+  console.log("Notification: ", e);
+  if (e.type === "dismissal") return;
+
+  var notification = new window.Notification(e.title, {
+    body: e.body,
+    icon: e.icon
   });
 
 };
